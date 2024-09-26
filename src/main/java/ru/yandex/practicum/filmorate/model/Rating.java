dde3cf463d5c3b352039e54ceab43ca8583c6c52
SergKostarev <kostarevsergo@yandex.ru>
2024-09-26 22:00:29 +0300

    Fix codestyle again

--- src/main/java/ru/yandex/practicum/filmorate/model/Rating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Rating.java
@@ -1,6 +1,4 @@
 package ru.yandex.practicum.filmorate.model;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
